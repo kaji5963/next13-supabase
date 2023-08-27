b60113f8749cdee2d1433fed51af865a656d86c9
takuya_kajiwara <fd.elt.119@gmail.com>
2023-08-27 21:04:39 +0900

    fetch API実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import NotesList from './components/notes-list'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="m-10 text-center">Hello World</div>
+      <div className="m-10 text-center">
+        <p>Hello World</p>
+        <NotesList />
+      </div>
     </main>
   )
 }
